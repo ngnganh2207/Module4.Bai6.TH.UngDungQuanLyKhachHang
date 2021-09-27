fa84f902fbc848888e862b9a2bccbf468ea3e42a
ngnganh2207 <ngocanhnguyen2207@gmail.com>
2021-09-27 16:36:15 +0700

    27.9.21

---
+++ src/main/java/com/codegym/repository/impl/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.repository.impl;
+
+import com.codegym.model.Customer;
+import com.codegym.repository.IGeneralRepositoy;
+
+public interface ICustomerRepository extends IGeneralRepositoy<Customer> {
+}
